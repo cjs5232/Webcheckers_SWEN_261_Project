51398ebfb7f2b6727f9806732aa75b6a4f5865a9
DavidArthurCole <40234707+DavidArthurCole@noreply.github.com>
2021-10-04 21:19:05 -0400

    Added JavaDoc for Piece Class, refactored fields to be private - accessed through getters/setters

--- src/main/java/com/webcheckers/util/Piece.java
+++ src/main/java/com/webcheckers/util/Piece.java
@@ -1,28 +1,52 @@
 package com.webcheckers.util;
 
 public class Piece {
+    /**
+     * Enumeration for piece Type
+     */
     public enum Type {
         SINGLE,
         KING,
     }
+    /**
+     * Enumueration for piece Color
+     */
     public enum Color {
         RED,
         WHITE
     }
 
-    public Type type;
-    public Color color;
+    private Type type;
+    private Color color;
 
+    /**
+     * Initialize a piece given a {@code Color} and {@code Type}
+     * @param type The type of the piece
+     * @param color The color of the piece
+     * 
+     * @see Type
+     * @see Color
+     */
     public Piece (Type type, Color color) {
         this.type = type;
         this.color = color;
     }
 
+    /**
+     * @return {@code Type} of piece
+     * 
+     * @see Type
+     */
     public Type getType() {
-        return type;
+        return this.type;
     }
 
+    /**
+     * @return {@code Color} of piece
+     * 
+     * @see Color
+     */
     public Color getColor() {
-        return color;
+        return this.color;
     }
 }
