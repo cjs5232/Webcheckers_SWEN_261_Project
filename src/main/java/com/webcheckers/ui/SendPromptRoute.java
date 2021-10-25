84fdc915a93a27916f8f370b716850f19893c890
DavidArthurCole <40234707+DavidArthurCole@noreply.github.com>
2021-10-25 15:48:45 -0400

    Added two empty routes for sending and accepting prompts, will be filling these in shortly

---
+++ src/main/java/com/webcheckers/ui/SendPromptRoute.java
@@ -0,0 +1,15 @@
+package com.webcheckers.ui;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class SendPromptRoute implements Route {
+
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
